dceefaaa218fada679be9f1fe7cf983bcf79fa17
Joona Viertola <git@joonaviertola.com>
2021-05-14 12:09:28 +0300

    Base build

---
+++ README.md
@@ -0,0 +1,1 @@
+# Norppa DS
---
+++ packages/button/src/button.ts
@@ -0,0 +1,22 @@
+import {LitElement, html} from 'lit';
+import {customElement} from 'lit/decorators.js';
+ 
+ /**
+  * Button component.
+  */
+ @customElement('norppa-button')
+ export class Button extends LitElement {
+   render() {
+     return html`
+       <button>
+         Button
+       </button>
+     `;
+   }
+ }
+ 
+declare global {
+  interface HTMLElementTagNameMap {
+    'norppa-button': Button;
+  }
+}
