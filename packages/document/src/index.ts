7a8460bae1ec300816b5d78d58ba5be50b36f6f6
Joona Viertola <git@joonaviertola.com>
2021-06-06 16:32:47 +0300

    Add component to doc page

--- packages/document/src/index.ts
+++ packages/document/src/index.ts
@@ -1,3 +1,19 @@
 import { Button } from 'norppa-button';
+import { Header } from 'norppa-header';
+import { html, render } from 'lit-html';
 
 new Button();
+new Header();
+
+// Define a template
+const myTemplate = () =>
+  html`
+    <div>
+      Hello, Norppa!
+      <norppa-header></norppa-header>
+      <norppa-button></norppa-button>
+    </div>
+  `;
+
+// Render the template to the document
+render(myTemplate(), document.body);
