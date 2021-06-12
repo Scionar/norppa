51ab74da1f4d42a2da6ffeb5297167dcab67832e
Joona Viertola <git@joonaviertola.com>
2021-06-12 14:17:24 +0300

    Add tag to card

--- packages/tag/src/tag.ts
+++ packages/tag/src/tag.ts
@@ -1,13 +1,23 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
-/** *
-Tag
-component. */ @customElement('norppa-tag')
+
+/**
+ * Tag component.
+ */
+@customElement('norppa-tag')
 export class Tag extends LitElement {
+  static styles = css`
+    :host {
+      background-color: var(--primary-color);
+      display: inline-block;
+    }
+  `;
+
   render() {
-    return html` <div><slot></slot></div> `;
+    return html`<div><slot /></div>`;
   }
 }
+
 declare global {
   interface HTMLElementTagNameMap {
     'norppa-tag': Tag;
